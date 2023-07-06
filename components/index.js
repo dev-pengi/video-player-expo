64bb8b35d49c0e352264aea60cee0d420d3d0d61
dev-pengi <siffffdoul@gmail.com>
2023-07-06 07:40:06 +0100

    Removed a component

--- components/index.js
+++ components/index.js
@@ -2,6 +2,5 @@ import Button from "./common/button/Button";
 import ScreenHeaderBtn from "./header/ScreenHeaderBtn";
 import VideoPlayer from "./videos/VideoPlayer/VideoPlayer";
 import VideoCard from "./common/cards/VideoCard/VideoCard";
-import ConfirmationModal from "./common/modal/ConfirmationModal";
 
-export { Button, ScreenHeaderBtn, VideoPlayer, VideoCard, ConfirmationModal };
+export { Button, ScreenHeaderBtn, VideoPlayer, VideoCard };
